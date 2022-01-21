571c570181843cff43703273bb0cde289f1f0818
Geordyn <geordyata@aol.com>
2022-01-21 09:38:59 -0500

    implemented a circle class

--- models/Shape/Circle.java
+++ models/Shape/Circle.java
@@ -1,13 +1,28 @@
 package models.Shape;
+/** radius function
+ * have radius property
+ * 
+ */
 
 public class Circle extends AbstractShape {
+    public double height;
+    public double width;
+    public double radius;
+
     public Circle(double height, double width) {
         super(height, width);
     }
-
+  
+public void CircleRadius(double height, double width, double radius) {
+    this.height = height;
+    this.width = width;
+    this.radius = radius;
+        
+}
     public double getArea(){
-        Double area = Math.pow(this.getHeight(), 2);
+        Double area = (Math.PI * Math.pow(this.radius, 2));
         return area;
     }
 
+  
 }
